b1238def94848e958b9feaafbf66708264a494bc
Noam_Levy <noamlv@mta.ac.il>
2019-05-20 14:49:41 +0300

    Client card and Garage classes were added

---
+++ Ex03.GarageLogic/ClientCard.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class ClientCard
+    {
+        public enum eState { onWork, finished, payed };
+
+        private string m_OwnerName;
+        private string m_OwnerCellPhone;
+        private Vehicle m_Vehicle;
+        private eState m_State = eState.onWork;
+
+        // properties:
+
+        public string OwnerName
+        {
+            get { return m_OwnerName; }
+            set { m_OwnerName = value; }
+        }
+
+        public string OwnerCellPhone
+        {
+            get { return m_OwnerCellPhone; }
+            set { m_OwnerCellPhone = value; }
+        }
+
+        public Vehicle Vehicle
+        {
+            get { return m_Vehicle; }
+            set { m_Vehicle = value; }
+        }
+
+        public eState State
+        {
+            get { return m_State; }
+            set { m_State = value; }
+        }
+
+        // methods:
+
+        public void InflateWheels(float i_Amount)
+        {
+            m_Vehicle.FillUpAirPressure();
+        }
+
+        public void FillEnergy(float i_Amount, int i_Type)
+        {
+            m_Vehicle.FillUpEnergy(i_Amount, i_Type);
+        }
+
+        public override string ToString()
+        {
+            return string.Format(@"Owner: {0}
+State: {1}
+Phone Number: {2}
+{3}", m_OwnerName, m_State, m_OwnerCellPhone, m_Vehicle.ToString());
+        }
+    }
+}
---
+++ Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Garage
+    {
+        private Dictionary<string, ClientCard> m_Clients= new Dictionary<string, ClientCard>();
+
+        public ClientCard FindByLicenceNum(string i_LicenceNum)
+        {
+            return m_Clients[i_LicenceNum];
+        }
+
+        public List<string> FindByState(int i_State)
+        {
+            List<string> filteredClients = new List<string>();
+            foreach(KeyValuePair<string, ClientCard> pair in m_Clients)
+            {
+                if(pair.Value.State == i_State)
+                {
+                    filteredClients.Add(pair.Key);
+                }
+            }
+
+            return filteredClients;
+        }
+
+        public List<string> getAllPlates()
+        {
+            List<string> plates = new List<string>();
+            foreach (KeyValuePair<string, ClientCard> pair in m_Clients)
+            {
+                    plates.Add(pair.Key);
+                
+            }
+
+            return plates;
+        }
+
+        public void Add(ClientCard i_ToAdd, string i_ClientName, string i_ClientPhone)
+        {
+            if (m_Clients.ContainsKey(i_ToAdd.Vehicle.LicenceNumber))
+            {
+                throw new Exception("Error: Vehicle already exsists.");
+            }
+
+            m_Clients.Add(i_ToAdd.LicenceNumber,i_ToAdd)
+        }
+    }
+}
